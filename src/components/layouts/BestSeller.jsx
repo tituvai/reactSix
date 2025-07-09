3a7a52161702641a77f02f64712f9d328233a36a
tituvai <titu17940@gmail.com>
2025-07-09 15:39:31 +0600

    newItem

--- src/components/layouts/BestSeller.jsx
+++ src/components/layouts/BestSeller.jsx
@@ -18,16 +18,16 @@ const BestSeller = () => {
                     <Hadding className={'text-[39px] text-hoverC font-bold pb-8'} text={'Bestseller'} as={'h3'} />
                 </div>
                 <Flex className={'justify-between'}>
-                    <div className="w-[24%]">
+                    <div className="w-[25%]">
                         <Product productSrc={bottol} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$3.34'}/>
                     </div>
-                    <div className="w-[24%]">
+                    <div className="w-[25%]">
                         <Product productSrc={whiteBag} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.04'}/>
                     </div>
-                    <div className="w-[24%]">
+                    <div className="w-[25%]">
                         <Product productSrc={cup} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$1.94'}/>
                     </div>
-                    <div className="w-[24%]">
+                    <div className="w-[25%]">
                         <Product productSrc={blackBag} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$5.00'}/>
                     </div>
                 </Flex>
