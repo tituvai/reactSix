11fbd3594a9c743ea4d9715257a88ad9b97c99f0
tituvai <titu17940@gmail.com>
2025-07-31 12:55:50 +0600

    slick item

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,10 +8,10 @@ import Compar from '../assets/icon/Compar';
 const Product = ({productSrc, newProduct, producttTitle, productPrice, productAlt}) => {
   return (
     <>
-        <div className=" relative group">
-            <Image className={'h-[300px]'} imgSrc={productSrc} imgAlt={productAlt}/>
-                {newProduct &&(<Hadding className={'py-2 px-8 bg-hoverC text-sm text-white font-bold absolute top-5 left-5'} text={newProduct} as={'h4'} />)}
-                <div className="p-7.5 bg-white absolute left-0 bottom-12 w-full opacity-0 group-hover:opacity-100 transition-all  ease-in-out translate-y-4 group-hover:translate-y-0 duration-700">
+        <div className=" relative group ">
+            <div className="relative">
+                <Image className={'h-[300px] bg-gray-200'} imgSrc={productSrc} imgAlt={productAlt}/>
+                 <div className="p-7.5 bg-white absolute left-0 bottom-0 w-full opacity-0 group-hover:opacity-100 transition-all  ease-in-out translate-y-4 group-hover:translate-y-0 duration-700">
                     <Flex className={'gap-2 justify-end'}>
                         <Hadding className={'hover:text-hoverC hover:font-bold  text-base text-menuC'} text={'Add to Wish List'} as={'p'} />
                         <FaHeart/>     
@@ -25,11 +25,14 @@ const Product = ({productSrc, newProduct, producttTitle, productPrice, productAl
                         <FaShoppingCart/>     
                     </Flex> 
                 </div>
+            </div>
+                {newProduct &&(<Hadding className={'py-2 px-8 bg-hoverC text-sm text-white font-bold absolute top-5 left-5'} text={newProduct} as={'h4'} />)}
+               
                 <div className="py-6 px-2">
-                   <Flex className={'justify-between'}>
-                    <Hadding className={'text-lg text-hoverC font-bold'} text={producttTitle} as={'h5'} />
+                   <div className={'flex justify-between'}>
+                    <Hadding className={'text-lg text-hoverC font-bold pl-2'} text={producttTitle} as={'h5'} />
                     <Hadding className={'text-base text-menuC'} text={productPrice} as={'p'} />
-                   </Flex>
+                   </div>
                 </div>
         </div>
     </>
--- src/components/layouts/Arrival.jsx
+++ src/components/layouts/Arrival.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../Product'
 import Container from '../Container'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-import time from '/src/assets/timeBox.png'
-import cat from '/src/assets/cat.png'
-import juri from '/src/assets/juri.png'
-import whact from '/src/assets/whact (2).png'
+// import time from '/src/assets/timeBox.png'
+// import cat from '/src/assets/cat.png'
+// import juri from '/src/assets/juri.png'
+// import whact from '/src/assets/whact (2).png'
 import Hadding from '../Hadding';
 import NextArrow from '../NextArrow';
 import PrevArrow from '../PrevArrow';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Arrival = () => {
 
@@ -23,6 +25,20 @@ const Arrival = () => {
     nextArrow:<NextArrow/>,
     prevArrow:<PrevArrow/>
   };
+
+//   Product Patr Start 
+
+  const [myProduct, setMyProduct] = useState([])
+
+  useEffect(()=>{
+
+   async function allProduct(){
+        let data = await axios.get('https://dummyjson.com/products');
+        setMyProduct(data.data.products)
+    }
+    allProduct()
+  })
+//   Product Patr End
   return (
     <>
         <div className="py-15">
@@ -32,21 +48,13 @@ const Arrival = () => {
                 </div>
                <div className="-mx-2">
                  <Slider {...settings }>
-                    <div className='px-2'>
-                        <Product productSrc={time} newProduct={'10%'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
+                    {myProduct.map((item)=>(
+                        <div className='px-2'>
+                        <Link to={'/'}><Product productSrc={item.thumbnail} newProduct={'New'} producttTitle={item.title} productPrice={'$5.400'}/></Link>
                     </div>
-                    <div className='px-2'>
-                        <Product productSrc={cat} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
-                    </div>
-                    <div className='px-2'>
-                        <Product productSrc={juri} newProduct={'50%'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
-                    </div>
-                    <div className='px-2'>
-                        <Product productSrc={time} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
-                    </div> 
-                    <div className='px-2'>
-                        <Product productSrc={whact} newProduct={'20%'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
-                    </div> 
+                    )) }
+                    
+                 
                 </Slider>
                </div>
             </Container>
@@ -56,3 +64,18 @@ const Arrival = () => {
 }
 
 export default Arrival
+
+
+
+//    <div className='px-2'>
+//                         <Product productSrc={cat} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
+//                     </div>
+//                     <div className='px-2'>
+//                         <Product productSrc={juri} newProduct={'50%'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
+//                     </div>
+//                     <div className='px-2'>
+//                         <Product productSrc={time} newProduct={'New'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
+//                     </div> 
+//                     <div className='px-2'>
+//                         <Product productSrc={whact} newProduct={'20%'} producttTitle={'Basic Crew Neck Tee'} productPrice={'$4.400'}/>
+//                     </div> 
